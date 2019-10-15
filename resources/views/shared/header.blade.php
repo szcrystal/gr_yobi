a86f45d07627d6227dfe303a31cc24485ec735d5
szcrystal <szk.create@gmail.com>
2019-10-15 13:40:28 +0000

    grbeta

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -5,11 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    {{--
     @if(! Ctm::isEnv('product'))
     <meta name="robots" content="noindex, nofollow">
     @endif
-    --}}
+    
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
