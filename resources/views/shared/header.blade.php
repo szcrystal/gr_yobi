24846066dfcf40d54de0b766d820e30f20e8030c
szcrystal <szk.create@gmail.com>
2019-12-12 14:30:56 +0000

    grbeta

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -33,13 +33,11 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     
 	@if(isset($isTop) || Request::is('item/*'))
-    {{--
-    <link href="{{ asset('cdn/slick-theme.min.css') }}" rel="stylesheet">{{-- 1.8.1 --}}
-    <link href="{{ asset('cdn/slick.min.css') }}" rel="stylesheet">
-    --}}
+    {{-- <link href="{{ asset('cdn/slick-theme.min.css') }}" rel="stylesheet"> --}}{{-- 1.8.1 --}}
+    {{-- <link href="{{ asset('cdn/slick.min.css') }}" rel="stylesheet"> --}}
     
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
 	@endif
     
     @if(! Ctm::isAgent('sp') && Request::is('item/*'))
